08a809c6c5a42c09b693c73c1f71d504e1e3633b
xuanchen liu <jasonliu97424@gmail.com>
2020-03-27 23:47:44 -0400

    init commit

---
+++ garbageSort/Controllers/ScanViewController.swift
@@ -0,0 +1,127 @@
+
+
+import UIKit
+import CoreML
+import Vision
+import SwiftyJSON
+import Alamofire
+import SDWebImage
+
+
+class ScanViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+    var pickedImage : UIImage?
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var Ntitle: UINavigationItem?
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        let userPickedImage = info[.editedImage]
+//        if(UIImagePickerController.isSourceTypeAvailable(.camera)){
+//            imageView.image = userPickedImage as? UIImage
+//        }
+//
+//        imagePicker.dismiss(animated: true, completion: nil)
+        if let userPickedImage = info[.originalImage] as? UIImage {
+                   
+                   guard let ciImage = CIImage(image: userPickedImage) else {
+                       fatalError("Could not convert image to CIImage.")
+                   }
+                   
+                   pickedImage = userPickedImage
+
+                   detect(flowerImage: ciImage)
+
+                    imageView.image = userPickedImage
+            
+               }
+        
+               
+               imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(flowerImage: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: GarbageClassifier().model) else {
+            fatalError("Can't load model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not complete classfication")
+            }
+            self.Ntitle?.title = result.identifier.capitalized
+            self.requestInfo(flowerName: result.identifier)
+        }
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String) {
+        let parameters : [String:String] = ["format" : "json", "action" : "query", "prop" : "extracts|pageimages", "exintro" : "", "explaintext" : "", "titles" : flowerName, "redirects" : "1", "pithumbsize" : "500", "indexpageids" : ""]
+        
+        
+        Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+
+                let flowerJSON : JSON = JSON(response.result.value!)
+                
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+            
+                self.label.text = flowerDescription
+                
+                
+                
+                
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL), completed: { (image, error,  cache, url) in
+                    
+                    if let currentImage = self.imageView.image {
+                        
+//                        guard let dominantColor = ColorThief.getColor(from: currentImage) else {
+//                            fatalError("Can't get dominant color")
+//                        }
+//
+                        
+                        DispatchQueue.main.async {
+                            self.navigationController?.navigationBar.isTranslucent = true
+                            
+                        }
+                    } else {
+                        self.imageView.image = self.pickedImage
+                        self.label.text = "Could not get information from Wikipedia."
+                    }
+                })
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                self.label.text = "Connection Issues"
+            }
+        }
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+         present(imagePicker, animated: true, completion: nil)
+    }
+}
+
---
+++ garbageSort/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garbageSort
+//
+//  Created by Shiqi Sheng on 2020-03-09.
+//  Copyright © 2020 Xuanchen Liu. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBAction func unwindToHomeVC(sender : UIStoryboardSegue){
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+
+}
