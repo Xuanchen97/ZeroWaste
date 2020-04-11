19b36670cc80d04c472cfe517fdeccc8457337ed
Zoe <wanhaoyu@sheridancollege.ca>
2020-04-11 15:45:14 -0400

    Add app delegate for sqlite db and functions for register

--- garbageSort/Classes/AppDelegate.swift
+++ garbageSort/Classes/AppDelegate.swift
@@ -7,16 +7,141 @@
 //
 
 import UIKit
+import SQLite3
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var databaseName : String? = "project.db"
+    var databasePath : String?
+    var people : [MyData] = []
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let documentPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentDir = documentPaths[0]
+        databasePath = documentDir.appending("/" + databaseName!)
+        
+        checkAndCreateDB()
+        readDataFromDB()
+        
         return true
+    }
+    
+    func insertIntoDB(person: MyData) -> Bool
+    {
+        var db : OpaquePointer? = nil
+        var returnCode : Bool = true
+        
+        if(sqlite3_open(self.databasePath, &db) == SQLITE_OK)
+        {
+            var insertStatement : OpaquePointer? = nil
+            var insertStatementString : String? = "insert into zw values(NULL, ?, ?, ?, ?)"
+            
+            if(sqlite3_prepare(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK)
+            {
+                let nameStr = person.name as NSString
+                let pwdStr = person.pwd as NSString
+                let avatarStr = person.avatar as NSString
+                let emailStr = person.email as NSString
+                
+                sqlite3_bind_text(insertStatement, 1, avatarStr.utf8String, -1, nil)
+                sqlite3_bind_text(insertStatement, 2, nameStr.utf8String, -1, nil)
+                sqlite3_bind_text(insertStatement, 3, pwdStr.utf8String, -1, nil)
+                sqlite3_bind_text(insertStatement, 4, emailStr.utf8String, -1, nil)
+                
+                if(sqlite3_step(insertStatement) == SQLITE_DONE)
+                {
+                    let rowId = sqlite3_last_insert_rowid(db)
+                    print("Success inserted row \(rowId)")
+                }else
+                {
+                    print("Could not insert row")
+                    returnCode = false
+                }
+                sqlite3_finalize(insertStatement)
+            }else
+            {
+                print("Insert statement could not be prepared")
+                returnCode = false
+            }
+            sqlite3_close(db)
+        }else
+        {
+            print("Unable to open database")
+            returnCode = false
+        }
+        
+        return returnCode
+    }
+    
+    func readDataFromDB()
+    {
+        people.removeAll()
+        
+        var db : OpaquePointer? = nil
+        
+        if (sqlite3_open(self.databasePath, &db) == SQLITE_OK)
+        {
+            print("Successfully open connection to database at \(self.databasePath)")
+            
+            var queryStatement : OpaquePointer? = nil
+            var queryStatementString : String? = "select * from zw"
+            
+            if (sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK)
+            {
+                while(sqlite3_step(queryStatement) == SQLITE_ROW)
+                {
+                    let id : Int = Int(sqlite3_column_int(queryStatement, 0))
+                    let cavatar = sqlite3_column_text(queryStatement, 1)
+                    let cname = sqlite3_column_text(queryStatement, 2)
+                    let cemail = sqlite3_column_text(queryStatement, 4)
+                    let cpwd = sqlite3_column_text(queryStatement, 3)
+                    
+                    let avatar = String(cString: cavatar!)
+                    let name = String(cString: cname!)
+                    let pwd = String(cString: cpwd!)
+                    let email = String(cString: cemail!)
+                    
+                    let data : MyData = MyData.init()
+                    data.initWithData(theRow: id, theAvatar: avatar, theName: name, thePwd: pwd, theEmail: email)
+                    people.append(data)
+                    
+                    print("Query result")
+                    print("\(id) | \(avatar) | \(name) | \(pwd) | \(email)")
+                    
+                }
+                sqlite3_finalize(queryStatement)
+            }else
+            {
+                print("Select statement could not be prepared")
+            }
+            sqlite3_close(db)
+        }else
+        {
+            print("Unable to open database")
+        }
+    }
+    
+    func checkAndCreateDB()
+    {
+        var success = false
+        let fileManager = FileManager.default
+        
+        success = fileManager.fileExists(atPath: databasePath!)
+        
+        if(success)
+        {
+            return
+        }
+        
+        let databasePathFromApp = Bundle.main.resourcePath?.appending("/" + databaseName!)
+        
+        try? fileManager.copyItem(atPath: databasePathFromApp!, toPath: databasePath!)
+        
+        return
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- garbageSort/Classes/MyData.swift
+++ garbageSort/Classes/MyData.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class MyData: NSObject {
+    var id : Int?
     var avatar : String = ""
     var name : String = ""
     var pwd : String = ""
     var email : String = ""
     
-    func initWithData(theAvatar a : String, theName n : String, thePwd p : String, theEmail e : String)
+    func initWithData(theRow i : Int, theAvatar a : String, theName n : String, thePwd p : String, theEmail e : String)
     {
+        id = i
         avatar = a
         name = n
         pwd = p
