1b679f03c7073e9598c1529791438968b979e185
Haoyue Wang <wanhaoyu@sheridancollege.ca>
2020-11-23 22:54:38 -0500

    add two sections in region table. table cell expandable

--- garbageSort/Controllers/SearchViewController.swift
+++ garbageSort/Controllers/SearchViewController.swift
@@ -13,20 +13,22 @@ class SearchViewController: UIViewController, UITableViewDataSource,
 
     @IBOutlet weak var tableView: UITableView!
     
-    let sections: [String] = ["Located region", "Regions", "Disposal rule"]
+    let sections: [String] = ["Located region", "Disposal rule"]
+    var s2Data: [String] = ["Toronto \n\nhttps://www.toronto.ca/services-payments/recycling-organics-garbage/\n\nGreen Cart: Eggs, Fruits, Vegetables, Meat, Diapers\nBlue Box: Metal, Glass, Cardboard, Plastic, Tissue paper\nGarbage: Everything else",
+                            "Halton \n\nhttps://www.halton.ca/For-Residents/Recycling-Waste/Waste-Recycling-Sorting-Guide\n\nGreen Cart: Eggs, Fruits, Vegetables, Meat, Tissue paper\nBlue Box: Metal, Glass, Cardboard, Plastic, Diapers\nGarbage: Everything else",
+                            "York",
+                            "Peel"]
     var mainDelegate = UIApplication.shared.delegate as! AppDelegate
-    var currentLoc = ""
     var sectionData: [Int: [String]] = [:]
+    var selectedIndex = -1
+    var isExpanded = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         let s1Data: [String] = ["\(mainDelegate.region)"]
-        let s2Data: [String] = ["Toronto", "Halton", "York", "Peel"]
-        let s3Data: [String] = ["Toronto", "Halton"]
+        sectionData = [0: s1Data, 1: s2Data]
         
-        sectionData = [0:s1Data, 1:s2Data, 2:s3Data]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
@@ -37,6 +39,14 @@ class SearchViewController: UIViewController, UITableViewDataSource,
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int)
             -> String? {
         return sections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if self.selectedIndex == indexPath.row && isExpanded == true && indexPath.section == 1{
+            return 256
+        }else{
+            return 48
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -52,8 +62,35 @@ class SearchViewController: UIViewController, UITableViewDataSource,
         }
             
         cell!.textLabel?.text = sectionData[indexPath.section]![indexPath.row]
-        
+        cell!.textLabel?.numberOfLines = 0
+        cell!.textLabel?.lineBreakMode = .byWordWrapping
+        //cell!.textLabel?.mar
         return cell!
     }
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+      
+        if indexPath.section == 1 {
+            
+            if selectedIndex == indexPath.row
+            {
+                if self.isExpanded == false
+                {
+                    self.isExpanded = true
+                }else{
+                    self.isExpanded = false
+                }
+            }else{
+                self.isExpanded = true
+            }
+        
+            
+            self.selectedIndex = indexPath.row
+            
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+    }
+    
 }
