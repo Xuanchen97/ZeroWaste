68f271cae763848a60ac5988b3cdef1d73111198
Haoyue Wang <wanhaoyu@sheridancollege.ca>
2020-12-02 00:43:25 -0500

    link

--- garbageSort/Controllers/SearchViewController.swift
+++ garbageSort/Controllers/SearchViewController.swift
@@ -16,10 +16,11 @@ class SearchViewController: UIViewController, UITableViewDataSource,
     @IBOutlet weak var tableView: UITableView!
     
     let sections: [String] = ["Located region", "Disposal rule"]
-    var s2Data: [String] = ["Toronto \n\nhttps://www.toronto.ca/services-payments/recycling-organics-garbage/\n\nGreen Cart: Eggs, Fruits, Vegetables, Meat, Diapers\nBlue Box: Metal, Glass, Cardboard, Plastic, Tissue paper\nGarbage: Everything else",
-                            "Halton \n\nhttps://www.halton.ca/For-Residents/Recycling-Waste/Waste-Recycling-Sorting-Guide\n\nGreen Cart: Eggs, Fruits, Vegetables, Meat, Tissue paper\nBlue Box: Metal, Glass, Cardboard, Plastic, Diapers\nGarbage: Everything else",
+    var s2Data: [String] = ["Toronto \n\nGreen Cart: Eggs, Fruits, Vegetables, Meat, Diapers\nBlue Box: Metal, Glass, Cardboard, Plastic, Tissue paper\nGarbage: Everything else",
+                            "Halton \n\nGreen Cart: Eggs, Fruits, Vegetables, Meat, Tissue paper\nBlue Box: Metal, Glass, Cardboard, Plastic, Diapers\nGarbage: Everything else",
                             "York",
                             "Peel"]
+    var linkData: [String] = ["https://www.toronto.ca/services-payments/recycling-organics-garbage/","https://www.halton.ca/For-Residents/Recycling-Waste/Waste-Recycling-Sorting-Guide","https://www.york.ca/wps/portal/yorkhome/environment/yr/garbageandrecycling/recyclingbluebox/!ut/p/z1/jZDbToQwEIafxQsupUP3QPWuwUNhRXYTdbE3pmC3kEBLCivq09usxsSo6FzNTL7_nwPiKEdci6daiaE2WjSuvufLh5hexoytIMnmJAIKGU1wSOB8MUPbAwC_BAXE_6OfAPi0ffLXAHcBtmmUKsQ7MVTHtd4ZlCthC6Gk0I9Wli9lU2uF8s-0aPayMM9uOX6wx3i-ZEEECQThCcQX19HNJsUY7uAbwDLigHC9OCMsgBX-AKYvUI0p3p9NdTEjblUrd9JK6--ta1fD0PWnHngwjqOvjFGN9EvTevCTpDL9gPKvJOra2_z1ikG9brekp0dvuMR2Bg!!/dz/d5/L2dBISEvZ0FBIS9nQSEh/#.X8cnSi2z2Rs","https://www.peelregion.ca/scripts/waste/how-to-sort-your-waste.pl?action=category&query=Recycle" ]
     var mainDelegate = UIApplication.shared.delegate as! AppDelegate
     var sectionData: [Int: [String]] = [:]
     var selectedIndex = -1
@@ -28,8 +29,6 @@ class SearchViewController: UIViewController, UITableViewDataSource,
     var postalCode = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-      
         
         let s1Data: [String] = ["\(mainDelegate.region)"]
         sectionData = [0: s1Data, 1: s2Data]
@@ -48,7 +47,7 @@ class SearchViewController: UIViewController, UITableViewDataSource,
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if self.selectedIndex == indexPath.row && isExpanded == true && indexPath.section == 1{
-            return 256
+            return 206
         }else{
             return 48
         }
@@ -69,14 +68,12 @@ class SearchViewController: UIViewController, UITableViewDataSource,
         cell!.textLabel?.text = sectionData[indexPath.section]![indexPath.row]
         cell!.textLabel?.numberOfLines = 0
         cell!.textLabel?.lineBreakMode = .byWordWrapping
-        //cell!.textLabel?.mar
+
         return cell!
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("search bar triggered!")
-        
-
     }
     
     func DismissKeyboard(){
@@ -166,6 +163,25 @@ class SearchViewController: UIViewController, UITableViewDataSource,
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true;
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        if indexPath.section == 1 {
+            let contextItem = UIContextualAction(style: .destructive, title: "View More") { (contextualAction, view, boolValue) in
+                if let url = URL(string: self.linkData[indexPath.row]) {
+                    UIApplication.shared.open(url)
+                }
+            }
+            let swipeActions = UISwipeActionsConfiguration(actions: [contextItem])
+            return swipeActions
+        }
+        else{
+            return nil
+        }
+   }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
